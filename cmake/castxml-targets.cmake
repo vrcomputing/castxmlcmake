787d1464e63816a3faf3045c32367bc0d505f266
Rick Vogel <vrick1991@googlemail.com>
2021-01-11 21:55:09 +0100

    fixed missing castxml executable check

--- cmake/castxml-targets.cmake
+++ cmake/castxml-targets.cmake
@@ -1,16 +1,17 @@
-if (WIN32)	
-	find_program(castxml_EXECUTABLE NAMES castxml PATHS ${castxml_DIR}/castxml-windows/castxml/bin NO_DEFAULT_PATH)
+if (WIN32)
+    find_program(castxml_EXECUTABLE NAMES castxml PATHS ${castxml_DIR}/castxml-windows/castxml/bin NO_DEFAULT_PATH)
 else()
-	find_program(castxml_EXECUTABLE NAMES castxml PATHS ${castxml_DIR}/castxml-linux/castxml/bin NO_DEFAULT_PATH)
+    find_program(castxml_EXECUTABLE NAMES castxml PATHS ${castxml_DIR}/castxml-linux/castxml/bin NO_DEFAULT_PATH)
 endif()
 
-if(NOT castxml_EXECUTABLE)
-	message(FATAL_ERROR "castxml_EXECUTABLE not found")
+if(EXISTS "${castxml_EXECUTABLE}")
+else()
+    message(FATAL_ERROR "castxml_EXECUTABLE not found")
 endif()
 
 add_executable(castxml IMPORTED)
-set_target_properties(castxml PROPERTIES 
-	IMPORTED_LOCATION_RELEASE        ${castxml_EXECUTABLE}
-	IMPORTED_LOCATION_DEBUG          ${castxml_EXECUTABLE}
-	IMPORTED_LOCATION_MINSIZEREL     ${castxml_EXECUTABLE}
-	IMPORTED_LOCATION_RELWITHDEBINFO ${castxml_EXECUTABLE})	
+set_target_properties(castxml PROPERTIES
+        IMPORTED_LOCATION_RELEASE        ${castxml_EXECUTABLE}
+        IMPORTED_LOCATION_DEBUG          ${castxml_EXECUTABLE}
+        IMPORTED_LOCATION_MINSIZEREL     ${castxml_EXECUTABLE}
+        IMPORTED_LOCATION_RELWITHDEBINFO ${castxml_EXECUTABLE})
